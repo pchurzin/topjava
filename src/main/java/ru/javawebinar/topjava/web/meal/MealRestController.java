803e8218623c57a461317322e08ba385b2ccf771
pchurzin <churzzz@gmail.com>
2018-07-11 21:05:37 +0300

    implemented meal rest controller

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,56 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
 public class MealRestController {
+    protected static final Logger log = getLogger(MealRestController.class);
+
     private MealService service;
 
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public List<MealWithExceed> getAll() {
+        log.info("get All");
+        return MealsUtil.getWithExceeded(service.getAll(authUserId()), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+    }
+
+    public MealWithExceed get(int id) {
+        log.info("get {}", id);
+        return getAll()
+                .stream()
+                .filter(mealWithExceed -> mealWithExceed.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public MealWithExceed create(Meal meal) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        service.create(meal, authUserId());
+        return get(meal.getId());
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id, authUserId());
+    }
+
+    public void update(Meal meal, int id) {
+        log.info("update {} with {}", meal, id);
+        assureIdConsistent(meal, id);
+        service.update(meal, authUserId());
+    }
 }
