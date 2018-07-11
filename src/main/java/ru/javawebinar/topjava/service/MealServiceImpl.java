fc8d8b72a6c620d831e73331a3e7a46547dd4936
pchurzin <churzzz@gmail.com>
2018-07-11 20:20:24 +0300

    implemented meal service

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,42 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.List;
 
 public class MealServiceImpl implements MealService {
 
     private MealRepository repository;
 
+    public MealServiceImpl(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Meal create(Meal meal, int userId) {
+        return repository.save(meal, userId);
+    }
+
+    @Override
+    public void delete(int id, int userId) throws NotFoundException {
+        ValidationUtil.checkNotFoundWithId(repository.delete(id, userId), id);
+    }
+
+    @Override
+    public Meal get(int id, int userId) throws NotFoundException {
+        return ValidationUtil.checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+    @Override
+    public void update(Meal meal, int userId) {
+        ValidationUtil.checkNotFoundWithId(repository.save(meal, userId), meal.getId());
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
 }
