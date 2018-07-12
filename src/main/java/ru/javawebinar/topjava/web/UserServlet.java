c1c062604f3041f991a65fcd485fcdccd869556f
pchurzin <churzzz@gmail.com>
2018-07-12 09:44:02 +0300

    implemented user login emulation

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -16,6 +16,11 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("forward to users");
+        int userId = SecurityUtil.authUserId();
+        try {
+            userId = Integer.parseInt(request.getParameter("userId"));
+            SecurityUtil.setAuthUserId(userId);
+        } catch (NumberFormatException e) {}
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
